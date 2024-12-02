0294e83433ff020d9fe4a71454cb7bf75d529bc8
hiroka <mitsunagahrk87@gmail.com>
2024-12-03 08:36:27 +0900

    21. spyOnを使用した関数のモック化

---
+++ src/section3/mock_spy.test.ts
@@ -0,0 +1,11 @@
+import { Calculator } from "./mock_spy";
+
+it("sumメソッドが呼び出される", () => {
+  const calculator = new Calculator();
+  //sumは監視対象の関数
+  const sumSpy = jest.spyOn(calculator, "sum");
+  const result = calculator.sum(1, 2);
+  expect(result).toBe(3);
+  expect(sumSpy).toHaveBeenCalledTimes(1);
+  expect(sumSpy).toHaveBeenCalledWith(1, 2);
+});
